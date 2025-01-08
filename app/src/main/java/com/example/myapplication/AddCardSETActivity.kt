305bfbcc7c3af46f26fec5577577c930bff16657
vova <vladyegorin@gmail.com>
2025-01-07 19:47:38 -0800

    activity for creating SETS of flashcards and button navigating to it from the main activity

---
+++ app/src/main/java/com/example/myapplication/AddCardSETActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class AddCardSETActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.addcardset)
+
+        //val backButton = findViewById<Button>(R.id.backButton)
+
+//        backButton.setOnClickListener {
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//        }
+    }
+
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -17,6 +18,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val button1 = findViewById<Button>(R.id.button1)
         val button2 = findViewById<Button>(R.id.button2)
+
+        button1.setOnClickListener {
+            val intent = Intent(this, AddCardSETActivity::class.java)
+            startActivity(intent)
+        }
+
 //        val textView1 = findViewById<TextView>(R.id.chigibam)
 //        val textView2 = findViewById<TextView>(R.id.secondtextview)
 //        // Set up the button click listener
