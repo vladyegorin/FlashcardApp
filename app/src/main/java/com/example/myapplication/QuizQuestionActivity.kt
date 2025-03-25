fc3384801257bc59ce9ff2f52351fd69ed669bea
vova <vladyegorin@gmail.com>
2025-03-25 15:34:06 -0700

    quiz activity done. next-> quiz results activity

--- app/src/main/java/com/example/myapplication/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/myapplication/QuizQuestionActivity.kt
@@ -14,12 +14,12 @@ class QuizQuestionActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.quizquestion)
 
-        val backButton = findViewById<Button>(R.id.backButton)
+        //val backButton = findViewById<Button>(R.id.backButton)
         val questionNumber = findViewById<TextView>(R.id.questionCount)
         val questionTextView = findViewById<TextView>(R.id.chigibam)
-        val answersResults = MutableList(5) { false }
+        //val answersResults = MutableList(5) { false }
         val nextButton = findViewById<Button>(R.id.nextButton)
-
+        var score = 0
         val questions = intent.getStringArrayExtra("questions")
         val answersList = intent.getSerializableExtra("answers") as? List<List<String>>
         val answers = answersList?.map { it.toTypedArray() }?.toTypedArray()
@@ -43,16 +43,27 @@ class QuizQuestionActivity: AppCompatActivity() {
 
         // Variable to keep track of the currently selected answer
         var selectedAnswer: CardView? = null // Initially no answer selected
+        var correctAnswer: CardView? = null
+        // find correct answer and drop the asterisk from the text
+        if(ans1.text[ans1.text.length - 1] == '*'){
+            correctAnswer = answer1
+            ans1.text = ans1.text.dropLast(1)
+
+        } else if(ans2.text[ans2.text.length - 1] == '*'){
+            correctAnswer = answer2
+            ans2.text = ans2.text.dropLast(1)
+        } else if(ans3.text[ans3.text.length - 1] == '*'){
+            correctAnswer = answer3
+            ans3.text = ans3.text.dropLast(1)
+        } else if(ans4.text[ans4.text.length - 1] == '*'){
+            correctAnswer = answer4
+            ans4.text = ans4.text.dropLast(1)
+        }
 
 
         answer1.setOnClickListener {
             // Deselect the previously selected answer
-            selectedAnswer?.setBackgroundColor(
-                ContextCompat.getColor(
-                    this,
-                    R.color.default_answer_color
-                )
-            )
+            resetAllAnswerCards()
 
             // Select the new answer
             selectedAnswer = answer1
@@ -62,12 +73,7 @@ class QuizQuestionActivity: AppCompatActivity() {
 
         answer2.setOnClickListener {
             // Deselect the previously selected answer
-            selectedAnswer?.setBackgroundColor(
-                ContextCompat.getColor(
-                    this,
-                    R.color.default_answer_color
-                )
-            )
+            resetAllAnswerCards()
 
             // Select the new answer
             selectedAnswer = answer2
@@ -77,12 +83,7 @@ class QuizQuestionActivity: AppCompatActivity() {
 
         answer3.setOnClickListener {
             // Deselect the previously selected answer
-            selectedAnswer?.setBackgroundColor(
-                ContextCompat.getColor(
-                    this,
-                    R.color.default_answer_color
-                )
-            )
+            resetAllAnswerCards()
 
             // Select the new answer
             selectedAnswer = answer3
@@ -92,56 +93,92 @@ class QuizQuestionActivity: AppCompatActivity() {
 
         answer4.setOnClickListener {
             // Deselect the previously selected answer
-            selectedAnswer?.setBackgroundColor(
-                ContextCompat.getColor(
-                    this,
-                    R.color.default_answer_color
-                )
-            )
+            resetAllAnswerCards()
 
             // Select the new answer
             selectedAnswer = answer4
             selectedAnswer?.setBackgroundResource(R.drawable.glow_effect) // Apply glow effect when selected
         }
+
+
         nextButton.setOnClickListener {
             if (questions != null && selectedAnswer != null) {
                 if (i < questions.size - 1) {
+
+                    if(selectedAnswer == correctAnswer){
+                        score++
+
+                    }
+                    resetAllAnswerCards()
                     i++
+
+
                     questionNumber.text = (i + 1).toString()
                     questionTextView.text = questions?.get(i)?.toString() ?: "Default Text"
                     ans1.text = answers?.getOrNull(i)?.getOrNull(0)?.toString() ?: "Default Text"
                     ans2.text = answers?.getOrNull(i)?.getOrNull(1)?.toString() ?: "Default Text"
                     ans3.text = answers?.getOrNull(i)?.getOrNull(2)?.toString() ?: "Default Text"
                     ans4.text = answers?.getOrNull(i)?.getOrNull(3)?.toString() ?: "Default Text"
-                    selectedAnswer?.setBackgroundColor(
-                        ContextCompat.getColor(
-                            this,
-                            R.color.default_answer_color
-                        )
-                    )
+                    if(i == 4){
+                        nextButton.text = "Finish"
+                        val intent = Intent(this, QuizResultsActivity::class.java)
+                        intent.putExtra("score", score)
+                        startActivity(intent)
+
+                    }
+                    if(ans1.text[ans1.text.length - 1] == '*'){
+                        correctAnswer = answer1
+                        ans1.text = ans1.text.dropLast(1)
+
+                    } else if(ans2.text[ans2.text.length - 1] == '*'){
+                        correctAnswer = answer2
+                        ans2.text = ans2.text.dropLast(1)
+                    } else if(ans3.text[ans3.text.length - 1] == '*'){
+                        correctAnswer = answer3
+                        ans3.text = ans3.text.dropLast(1)
+                    } else if(ans4.text[ans4.text.length - 1] == '*'){
+                        correctAnswer = answer4
+                        ans4.text = ans4.text.dropLast(1)
+                    }
+
+
                 }
             }
 
 
         }
-        backButton.setOnClickListener {
-            if (questions != null && selectedAnswer != null) {
-                if (i > 0) {
-                    i--
-                    questionNumber.text = (i + 1).toString()
-                    questionTextView.text = questions?.get(i)?.toString() ?: "Default Text"
-                    ans1.text = answers?.getOrNull(i)?.getOrNull(0)?.toString() ?: "Default Text"
-                    ans2.text = answers?.getOrNull(i)?.getOrNull(1)?.toString() ?: "Default Text"
-                    ans3.text = answers?.getOrNull(i)?.getOrNull(2)?.toString() ?: "Default Text"
-                    ans4.text = answers?.getOrNull(i)?.getOrNull(3)?.toString() ?: "Default Text"
-                    selectedAnswer?.setBackgroundColor(
-                        ContextCompat.getColor(
-                            this,
-                            R.color.default_answer_color
-                        )
-                    )
-                }
-            }
-        }
+//        backButton.setOnClickListener {
+//            if (questions != null && selectedAnswer != null) {
+//                if (i > 0) {
+//                    i--
+//                    questionNumber.text = (i + 1).toString()
+//                    questionTextView.text = questions?.get(i)?.toString() ?: "Default Text"
+//                    ans1.text = answers?.getOrNull(i)?.getOrNull(0)?.toString() ?: "Default Text"
+//                    ans2.text = answers?.getOrNull(i)?.getOrNull(1)?.toString() ?: "Default Text"
+//                    ans3.text = answers?.getOrNull(i)?.getOrNull(2)?.toString() ?: "Default Text"
+//                    ans4.text = answers?.getOrNull(i)?.getOrNull(3)?.toString() ?: "Default Text"
+//                    selectedAnswer?.setBackgroundColor(
+//                        ContextCompat.getColor(
+//                            this,
+//                            R.color.default_answer_color
+//                        )
+//                    )
+//                }
+//            }
+//        }
+
     }
+    private fun resetAllAnswerCards() {
+        val answer1: CardView = findViewById(R.id.answer1)
+        val answer2: CardView = findViewById(R.id.answer2)
+        val answer3: CardView = findViewById(R.id.answer3)
+        val answer4: CardView = findViewById(R.id.answer4)
+
+        answer1.setBackgroundResource(R.drawable.quiz_answer_background)
+        answer2.setBackgroundResource(R.drawable.quiz_answer_background)
+        answer3.setBackgroundResource(R.drawable.quiz_answer_background)
+        answer4.setBackgroundResource(R.drawable.quiz_answer_background)
+    }
+
 }
+
